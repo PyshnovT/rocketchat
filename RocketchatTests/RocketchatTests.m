bffe8bf59ec20874522773239ebce6e5c241b2a4
Timothy Pyshnov <lightwight99@gmail.com>
2015-08-22 18:17:41 +0300

    Adding xCode Project

---
+++ RocketchatTests/RocketchatTests.m
@@ -0,0 +1,40 @@
+//
+//  RocketchatTests.m
+//  RocketchatTests
+//
+//  Created by Тимофей Пышнов on 22/08/15.
+//  Copyright (c) 2015 Pyshnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RocketchatTests : XCTestCase
+
+@end
+
+@implementation RocketchatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
